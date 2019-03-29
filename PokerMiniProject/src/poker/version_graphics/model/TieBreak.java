351f8282edc2ab1d844ee2e8f28ad830b2ee5821
Ruben Gonzalez <rubengaviles@gmail.com>
2019-03-29 12:52:25 +0100

    One Pair/ Straight / Flush / StraightFlushTie Break added

--- PokerMiniProject/src/poker/version_graphics/model/TieBreak.java
+++ PokerMiniProject/src/poker/version_graphics/model/TieBreak.java
@@ -49,12 +49,37 @@ public class TieBreak {
                 }
             }
         }
-
                 return definiteWinners;
     }
     private static ArrayList<Player> tieOnePair(ArrayList<Player> allWinners){
+        ArrayList<Player> definiteWinners = new ArrayList<>();
+        ArrayList<Integer> pairs = new ArrayList<>();
+        //go through each player hand and store the value of the pair in a Arraylist
+        for (Player x : allWinners) {
+            boolean found = false;
+            for (int i = 0; i < x.getCards().size() - 1 && !found; i++) {
+                for (int j = i + 1; j < x.getCards().size() && !found; j++) {
+                    if (x.getCards().get(i).getRank() == x.getCards().get(j).getRank()) {
+                        pairs.add(x.getCards().get(i).getRank().ordinal());
+                        found = true;
+                    }
 
-        return allWinners;
+                }
+            }
+        }
+        //find highest pair value
+        int highestValue = 0;
+        for(int i : pairs){
+            if(i>highestValue) highestValue = i;
+        }
+        //Find highest values in Arraylist and add player at the same index in definiteWinners
+        for(int i=0; i< pairs.size(); i++){
+            if(pairs.get(i)==highestValue){
+                definiteWinners.add(allWinners.get(i));
+            }
+        }
+
+        return definiteWinners;
     }
     private static ArrayList<Player> tieTwoPair(ArrayList<Player> allWinners){
 
@@ -69,8 +94,8 @@ public class TieBreak {
         return allWinners;
     }
     private static ArrayList<Player> tieStraight(ArrayList<Player> allWinners){
-
-        return allWinners;
+        //Evaluate players with highest card
+        return tieHighCard(allWinners);
     }
 
     private static ArrayList<Player> tieFullHouse(ArrayList<Player> allWinners){
@@ -79,13 +104,12 @@ public class TieBreak {
 
     }
     private static ArrayList<Player> tieFlush(ArrayList<Player> allWinners){
-
-        return allWinners;
+        //Highest card in Flush wins
+        return tieHighCard(allWinners);
     }
     private static ArrayList<Player> tieStraightFlush(ArrayList<Player> allWinners){
-
-        return allWinners;
-
+        //Evaluate players with highest card
+        return tieHighCard(allWinners);
     }
 
 }
